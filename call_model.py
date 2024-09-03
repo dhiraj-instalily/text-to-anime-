43b92cccfbe825dac72142204c970ecf7b7c0dd6
Dhiraj Khanal <dhiraj@instalily.ai>
2024-09-03 13:26:08 -0400

    env/config

--- call_model.py
+++ call_model.py
@@ -4,9 +4,9 @@ import base64
 from io import BytesIO
 from PIL import Image
 import os
+from config import URL, API_KEY, STATIC_FOLDER
 
-URL = 'https://m-2cdaff0852124e0c8659af40827a4996-m.default.model-v1.inferless.com/v2/models/Animagine-xl-3.1_2cdaff0852124e0c8659af40827a4996/versions/1/infer'
-headers = {"Content-Type": "application/json", "Authorization": "Bearer e0cee946ceedd0c7fbadee19f66c818de29fc42180cd2664ea0da5dae57cb088666df25ba07cf2e309639d342320fcb15d7ec8d14c38ff8d33cdcc1049197eea"}
+headers = {"Content-Type": "application/json", "Authorization": f"Bearer {API_KEY}"}
 
 def generate_image(prompt, negative_prompt):
     data = {
@@ -34,12 +34,11 @@ def generate_image(prompt, negative_prompt):
     image_buffer = BytesIO(image_data)
     image = Image.open(image_buffer)
 
-    static_folder = 'static'
-    if not os.path.exists(static_folder):
-        os.makedirs(static_folder)
+    if not os.path.exists(STATIC_FOLDER):
+        os.makedirs(STATIC_FOLDER)
     
     image_filename = f"generated_image_{hash(prompt)}.png"
-    image_path = os.path.join(static_folder, image_filename)
+    image_path = os.path.join(STATIC_FOLDER, image_filename)
     
     image.save(image_path)
     
